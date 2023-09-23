16dc1a49b9cf76239fe859bd95639541b802bd5e
유성원 <swyoo753@naver.com>
2023-09-23 23:43:21 +0900

    cors해결시도 1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   images: {
     domains: ["fastly.picsum.photos", "res.cloudinary.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://next-board-git-main-ysw753.vercel.app/:path*",
+      },
+    ];
   },
 };
 
